29e9f8d5026f6e7e810140daa4c898be4897f661
jaewon <jml6388@naver.com>
2026-03-12 20:14:49 +0900

    init: 듀토리얼 셋업

---
+++ RAG/index.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+
+// RAG (Retrieval-Augmented Generation) Tutorial
+// 문서를 벡터 DB에 저장하고, 질문에 관련 문서를 검색해서 답변하는 시스템
---
+++ SQL-agent/index.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+
+// SQL Agent Tutorial
+// 자연어 질문을 SQL 쿼리로 변환하여 DB에서 데이터를 조회하는 에이전트
